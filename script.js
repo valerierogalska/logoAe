3e7a455ea5139d7a14e047d3ee423990865a7793
valerierogalska <valerierogalska@gmail.com>
2018-11-27 19:05:57 +0100

    Added custom script

---
+++ script.js
@@ -0,0 +1,8 @@
+var animation = bodymovin.loadAnimation({
+  container: document.getElementById('lottie'), // Required
+  path: 'http://valerierogalska.com/data.json', // Required
+  renderer: 'svg', // Required
+  loop: true, // Optional
+  autoplay: true, // Optional
+  name: "Denial Logo", // Name for future reference. Optional.
+})
